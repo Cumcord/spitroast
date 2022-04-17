3ed139ee8221536a18d457ef53c1c5966fad0a33
creatable <cr3atable@riseup.net>
2022-04-17 17:33:16 -0400

    Remove redundant thing (getting off to a great start HAHAHA)

--- src/index.js
+++ src/index.js
@@ -5,13 +5,4 @@ const before = getPatchFunc("before");
 const instead = getPatchFunc("instead");
 const after = getPatchFunc("after");
 
-if (globalThis.module) {
-  globalThis.module.exports = {
-    before,
-    instead,
-    after,
-    unpatchAll
-  };
-}
-
 export { instead, before, after, unpatchAll };
