d8275f8041f4888ff4378e09d569b34d094cade7
creatable <cr3atable@riseup.net>
2022-04-18 08:03:11 -0400

    i did not mean to push the version with those things exposed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import getPatchFunc from "./getPatchFunc.js";
 import { unpatchAll } from "./unpatch.js";
-import { patches, patchedObjects } from "./shared.js";
 
 const before = getPatchFunc("before");
 const instead = getPatchFunc("instead");
 const after = getPatchFunc("after");
 
-export { instead, before, after, unpatchAll, patches, patchedObjects };
+export { instead, before, after, unpatchAll };
